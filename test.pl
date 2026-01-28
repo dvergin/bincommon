69405cbf626070921c11cc558c7e827c893847c3
David Vergin <dvergin@fastmail.net>
2026-01-27 19:38:07 -0800

    Updating test.pl

--- test.pl
+++ test.pl
@@ -1,9 +1,22 @@
 #!/usr/bin/env perl
 use strict;
 use warnings;
+use v5.38;   # enables: say, signatures, state, switch
 use Data::Dumper;    #print Dumper($var_ref)
-use feature 'say';
+use feature qw/say class/;
+no warnings 'experimental::smartmatch', 'experimental::class';
 
-my $n = 34;
-say "Yes" if $n =~ /4/;
+say 'yes';
+
+class Example {
+   field $x = 3;
+
+   method sayit {
+      say $x;
+   }
+}
+
+my $s = Example->new();
+
+$s->sayit();
 

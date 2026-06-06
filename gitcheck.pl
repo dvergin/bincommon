c0c2173f00250ff3b142bde51c0f1af7cc3b0234
David Vergin <dvergin@fastmail.net>
2026-06-05 19:20:08 -0700

    updated gitcheck.pl and config

--- gitcheck.pl
+++ gitcheck.pl
@@ -6,16 +6,39 @@ use v5.38;
 use Data::Dumper;    #print Dumper($var_ref)
 
 use lib '/home/dvergin/bincommon/perlmodules';
+use dv_util;
+my $util = dv_util->new();
 
-my @boxes = qw/amida raspi xps8700/;
+my $sendemail = defined $ARGV[0] && $ARGV[0] eq '--email' ? 1 : 0;
+
+my $report = '';
+my @boxes = qw/amida chromebook laptop raspi towerplus xps8700/;
 
 for my $box (@boxes) {
-   say "Checking git repo on $box";
-   my $resp = `ssh $box "cd bincommon; git fetch; git status"`;
+   $report .= "Checking git repo on $box\n";
+   my $resp = $box eq $ENV{'MACHINE'}
+            ? `cd ~/bincommon; git fetch; git status`
+            : `ssh $box "cd ~/bincommon; git fetch; git status"`;
+   $report .= "- - - -\n$resp- - - -\n";
    if ($resp =~ /(Your branch is behind.+)/) {
       my $alert = $1;
       $alert =~ s/Your branch/$box/;
-      say $alert;
+      $report .= "$alert\n";
+   } else {
+      $report .= "$box git is up to date\n";
    }
-   say '--------------------------------------------------------'
+   if ($resp =~ /Changes not staged/) {
+      $report .= "$box has unstaged changes\n";
+   }
+   $report .= "--------------------------------------------------------\n";
 }
+
+if ($sendemail) {
+   $util->email(to      => 'David Vergin <dvergin@fastmail.net>',
+                from    => 'Git Checker <dvergin@fastmail.net>',
+                subject => 'Git Status Report',
+                message => $report);
+} else {
+   say $report;
+}
+

cfb5726e22c5a914f80e3dba0136ff5ac7d48251
David Vergin <dvergin@fastmail.net>
2026-06-13 20:57:21 -0700

    new cretentialcheck.pl, modified gitcheck.pl

---
+++ credentialcheck.pl
@@ -0,0 +1,52 @@
+#!/usr/bin/env perl
+use strict;
+use warnings;
+use v5.38;
+
+use Data::Dumper;    #print Dumper($var_ref)
+
+use lib '/home/dvergin/bincommon/perlmodules';
+use dv_util;
+my $util = dv_util->new();
+use constant PROCESS_SUCCESS => 0;
+
+my $sendemail = defined $ARGV[0] && $ARGV[0] eq '--email' ? 1 : 0;
+
+my $report = '';
+my @boxes = qw/amida chromebook laptop raspi towerplus xps8700/;
+
+for my $box (@boxes) {
+   my $resp = system("ssh -o BatchMode=yes -o ConnectTimeout=2 -q $box exit");
+   if ($resp eq PROCESS_SUCCESS) {
+      say "$box is online";
+   } else {
+      say "$box is off line";
+   }
+   say "---------------------------------";
+
+   #$report .= "Checking git repo on $box\n";
+   #my $resp = $box eq $ENV{'MACHINE'}
+   #         ? `cd ~/bincommon; git fetch; git status`
+   #         : `ssh $box "cd ~/bincommon; git fetch; git status"`;
+   ##$report .= "- - - -\n$resp- - - -\n";
+   #if ($resp =~ /(Your branch is behind.+)/) {
+   #   my $alert = $1;
+   #   $alert =~ s/Your branch/$box/;
+   #   $report .= "$alert\n";
+   #} else {
+   #   $report .= "$box git is up to date\n";
+   #}
+   #if ($resp =~ /Changes not staged/) {
+   #   $report .= "$box has unstaged changes\n";}
+   #$report .= "---------------------------------------\n";
+}
+
+if ($sendemail) {
+   $util->email(to      => 'David Vergin <dvergin@fastmail.net>',
+                from    => 'Git Checker <dvergin@fastmail.net>',
+                subject => 'Git Status Report',
+                message => $report);
+} else {
+   say $report;
+}
+
--- gitcheck.pl
+++ gitcheck.pl
@@ -11,34 +11,77 @@ my $util = dv_util->new();
 
 my $sendemail = defined $ARGV[0] && $ARGV[0] eq '--email' ? 1 : 0;
 
-my $report = '';
-my @boxes = qw/amida chromebook laptop raspi towerplus xps8700/;
+my $short_report = '';
+my $full_report  = '';
+my %prob_details = ();
+my @okay_boxes = ();
+my @offline = ();
 
+my @boxes = qw/amida chromebook laptop raspi towerplus xps8700/;
 for my $box (@boxes) {
-   $report .= "Checking git repo on $box\n";
-   my $resp = $box eq $ENV{'MACHINE'}
-            ? `cd ~/bincommon; git fetch; git status`
-            : `ssh $box "cd ~/bincommon; git fetch; git status"`;
-   #$report .= "- - - -\n$resp- - - -\n";
-   if ($resp =~ /(Your branch is behind.+)/) {
-      my $alert = $1;
-      $alert =~ s/Your branch/$box/;
-      $report .= "$alert\n";
+   say $box;
+   my $online = 0;
+   my $git_test = 'cd ~/bincommon; git fetch; git status';
+   if ($box eq $ENV{'MACHINE'}) {
+      $online = 1;
    } else {
-      $report .= "$box git is up to date\n";
+      $git_test = "ssh $box " . "'$git_test'";
+      my $status_check = system("ssh -o BatchMode=yes -o ConnectTimeout=2 -q $box exit");
+      if ($status_check == 0) {
+         $online = 1;
+      } else {
+         push(@offline, $box);
+      }
    }
-   if ($resp =~ /Changes not staged/) {
-      $report .= "$box has unstaged changes\n";
+   if ($online) {
+      my $git_status_report = `$git_test`;
+      $full_report .= "-------------------\n$box\n$git_test\n$git_status_report\n------------------\n";
+      my @rpt_ary = ();
+      if ($git_status_report =~ /(Your branch is behind.+)/) {
+         push(@rpt_ary, 'branch is behind');
+      } else {
+         
+      }
+      if ($git_status_report =~ /Untracked files/) {
+         push(@rpt_ary, 'untracked files');
+      }
+      if ($git_status_report =~ /Changes not staged/) {
+         push(@rpt_ary, '/ unstaged changes');
+      }
+      if ($git_status_report =~ /Changes to be committed/) {
+         push(@rpt_ary, '/ changes to be committed');
+      }
+      if (@rpt_ary) {
+         $prob_details{$box} = \@rpt_ary;
+      } else {
+         push(@okay_boxes, $box);
+      }
    }
-   $report .= "--------------------------------------------------------\n";
 }
+if (@offline) {
+   $short_report .= 'Off-line: ' . join( ' ', @offline) . "\n"; }
+if (@okay_boxes) {
+   for my $okay_box (@okay_boxes) {
+      $short_report .= "$okay_box is okay\n";
+   }
+}
+if (%prob_details) {
+   for my $box (@boxes) {
+      if ( exists $prob_details{$box} ) {
+         $short_report .= "$box: " . join( ' ', @{ $prob_details{$box} } ) . "\n";
+      }
+   }
+}
+
+my $email_report = $short_report;
+$email_report .= $full_report;
 
 if ($sendemail) {
    $util->email(to      => 'David Vergin <dvergin@fastmail.net>',
                 from    => 'Git Checker <dvergin@fastmail.net>',
                 subject => 'Git Status Report',
-                message => $report);
+                message => $email_report);
 } else {
-   say $report;
+   say $email_report;
 }
 
